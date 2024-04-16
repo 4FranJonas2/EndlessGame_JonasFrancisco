983d7f6a84ad4b1accac8336ef3007c1d32bee09
4FranJonas2 <franjonas98@hotmail.com>
2024-04-16 16:49:13 -0300

    Add mainCharacter a Rectangle

--- EndlessGame_FranciscoJonas/main.cpp
+++ EndlessGame_FranciscoJonas/main.cpp
@@ -5,9 +5,16 @@ using namespace std;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    //Me crea la ventana
+    sf::RenderWindow window(sf::VideoMode(600, 600), "SFML EndlessGame Francisco Jonas");
+   //Crea un circulo
+    sf::RectangleShape rectangle (sf::Vector2f(100.f,50.f));
+    rectangle.setSize(sf::Vector2f(100.f, 100.f));
+    rectangle.setFillColor(sf::Color(150,50,250));
+    // set shape
+    rectangle.setOutlineThickness(20.f);
+    rectangle.setOutlineColor(sf::Color(250, 150, 100));
+
 
     while (window.isOpen())
     {
@@ -19,7 +26,7 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(rectangle);
         window.display();
     }
 
