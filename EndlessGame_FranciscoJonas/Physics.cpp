2566ee303bdd8fd457b67f04f25b828e60857ce2
4FranJonas2 <132166496+4FranJonas2@users.noreply.github.com>
2024-06-11 16:02:46 -0300

    unnecesary declaration on cpp fixed

--- EndlessGame_FranciscoJonas/Physics.cpp
+++ EndlessGame_FranciscoJonas/Physics.cpp
@@ -1,41 +1,33 @@
 #include "Physics.h"
 
-class Gravity {
 
-private:
-    sf::RectangleShape m_shape;
-    sf::Vector2f position, velocity, acceleration;
+Gravity::Gravity(float x_pos, float y_pos) : position (x_pos, y_pos) 
+{
+    m_shape.setSize(sf::Vector2f(100, 100));
+    m_shape.setFillColor(sf::Color::Black);
+    m_shape.setPosition(sf::Vector2f(x, y));
 
-public:
+    // Constants at the moment (initial velocity up, then gravity pulls down)
+    velocity = sf::Vector2f(0, -30);
+    acceleration = sf::Vector2f(0, 9.81); //Earth's acceleration
+}
 
-    Gravity::Gravity(float x_pos, float y_pos) : position (x_pos, y_pos) 
+void Gravity::Step()
+{ // TODO: use delta time
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
     {
-        m_shape.setSize(sf::Vector2f(100, 100));
-        m_shape.setFillColor(sf::Color::Black);
-        m_shape.setPosition(sf::Vector2f(x, y));
-
-        // Constants at the moment (initial velocity up, then gravity pulls down)
-        velocity = sf::Vector2f(0, -30);
-        acceleration = sf::Vector2f(0, 9.81); //Earth's acceleration
+        velocity.y -= 1;
     }
 
-    void Gravity::Step()
-    { // TODO: use delta time
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-        {
-            velocity.y -= 1;
-        }
+    velocity.x += acceleration.x;
+    velocity.y += acceleration.y;
+    x += velocity.x;
+    y += velocity.y;
 
-        velocity.x += acceleration.x;
-        velocity.y += acceleration.y;
-        x += velocity.x;
-        y += velocity.y;
+    m_shape.setPosition(x, y);
+}
 
-        m_shape.setPosition(x, y);
-    }
-
-    void Gravity::Draw(sf::RenderWindow& window)
-    {
-        window.draw(m_shape);
-    }
+void Gravity::Draw(sf::RenderWindow& window)
+{
+    window.draw(m_shape);
 }
